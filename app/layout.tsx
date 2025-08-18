a0b0bf4191a9686ab0ba2a3f0ace9641eabb3134
Balmeet-98 <balmeetsingh44@gmail.com>
2025-08-19 04:12:15 +0530

    google analytic add

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next';
 import './globals.css'
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -31,6 +32,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+<Script async src="https://www.googletagmanager.com/gtag/js?id=G-V7MME7KLY3"></Script>
+<Script>
+{`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V7MME7KLY3');`}
+</Script>
         <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
           {children}
           <Analytics />
